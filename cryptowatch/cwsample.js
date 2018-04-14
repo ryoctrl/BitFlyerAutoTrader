b1daf3271dbc690b28507f81ae4115f71af933d8
tonaspace <tonaspace@gmail.com>
2018-04-14 17:16:29 +0900

    CryptoWatchからデータを取ってくる機能と例

---
+++ cryptowatch/cwsample.js
@@ -0,0 +1,28 @@
+var CwConfig = require('./cwconfig.js');
+var CwUtil = require('./cwutil.js');
+var moment = require('moment');
+
+async function cwSample () {
+  var candles1 = await CwUtil.getOhlc(1, 10);
+  console.log(candles1);
+  var candles5 = await CwUtil.getOhlc(5, 10);
+  console.log(candles5);
+  var candles15 = await CwUtil.getOhlc(15, 10);
+  console.log(candles15);
+}
+// cwSample();
+
+// 5分足の高値安値を過去10本分とってくる
+async function highLowSample () {
+  var candles = await CwUtil.getOhlc(5, 10);
+  var timestamps = CwUtil.getTimes(candles)
+  var highs = CwUtil.getHighs(candles)
+  var lows = CwUtil.getLows(candles)
+  console.log(moment(timestamps[i]))
+  for (var i = 0; i < candles.length; i++) {
+    t = moment(timestamps[i]).format(('YYYY/MM/DD HH:mm:ss'));
+    console.log('終了時刻:', t, '高値:', highs[i], '安値:', lows[i]);
+  }
+}
+
+highLowSample();
---
+++ cryptowatch/cwutil.js
@@ -0,0 +1,45 @@
+// cryptowatchからn分足のOHLCを、過去hist本分取得する
+var r2 = require('r2');
+async function getOhlc(n, hist) {
+  var now = Math.ceil((new Date()).getTime() / 1000); // 秒単位
+  var beginTime = now - (n * 60) * hist;
+  n_sec = n * 60;
+  // cryptowatch format
+  url = 'https://api.cryptowat.ch/markets/bitflyer/btcfxjpy/ohlc?periods=' + n_sec + '&after=' + beginTime
+  var options = {
+    url: url,
+    method: 'GET',
+    body: null,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  var response = await r2(url).json;
+  var candleStr = String(n_sec)
+  // 最初と最後は不要なので捨てる
+  return response['result'][candleStr].slice(1, -1);
+}
+
+module.exports.getOhlc = getOhlc;
+
+// CryptoWatchから来る形式を使いやすいように変換
+exports.getTimes = function (ohlc_list) {
+  return ohlc_list.map(ohlc => ohlc[0] * 1000);
+}
+
+exports.getOpens = function (ohlc_list) {
+  return ohlc_list.map(ohlc => ohlc[1]);
+}
+
+exports.getHighs = function (ohlc_list) {
+  return ohlc_list.map(ohlc => ohlc[2]);
+}
+
+exports.getLows = function (ohlc_list) {
+  return ohlc_list.map(ohlc => ohlc[3])  
+}
+
+exports.getCloses = function (ohlc_list) {
+  return ohlc_list.map(ohlc => ohlc[4]);
+}
+
