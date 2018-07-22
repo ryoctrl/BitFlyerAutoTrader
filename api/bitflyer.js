dda5c5a9b6979d28a7c09f8a37172cf16d084573
DESKTOP-Q5KJIK8\mosin <ryoctrl@gmail.com>
2018-07-22 23:02:11 +0900

    APIモジュールのAPIキーを参照元から渡すよう変更/メインbotの乖離率をリアルタイムAPIによる算出に変更

--- api/bitflyer.js
+++ api/bitflyer.js
@@ -3,119 +3,124 @@ const fetch = require('node-fetch');
 const crypto = require('crypto');
 const Secret = require('../secret.json');
 
-const API_KEY = Secret.API_KEY;
-const API_SECRET = Secret.API_SECRET;
 const URL = 'https://api.bitflyer.jp';
 
 class BitFlyer {
-	constructor() {
-	}
-	
-	/* Public Methods*/
-	//資産残高を取得
-	async getBalances() {
-		let method = 'GET';
-		let path = '/v1/me/getbalance';
-		return await this.sendRequest(method, path, null, true);
-	}
-	
-	//証拠金状態を取得
-	async getCollateral() {
-		let method = 'GET';
-		let path = '/v1/me/getcollateral';
-		return await this.sendRequest(method, path, null, true);
-	}
-	
-	//証拠金変動履歴を取得
-	async getCollateralHistory() {
-		let method = 'GET';
-		let path = '/v1/me/getcollateralhistory';
-		return await this.sendRequest(method, path, null, true);
-	}
+    constructor(API_KEY, API_SECRET) {
+        this.API_KEY = API_KEY;
+        this.API_SECRET = API_SECRET;
+    }
 
-	//現在の建玉を取得
-	async getPositions() {
-		let method = 'GET';
-		let path = '/v1/me/getpositions?product_code=FX_BTC_JPY';
-		return await this.sendRequest(method, path, null, true);
-	}
-	
-	//注文を出す	
-	async sendChildorder(body) {
-		let method = 'POST';
-		let path = '/v1/me/sendchildorder';
-		return await this.sendRequest(method, path, body, true);
-	}
-	
-	//注文をキャンセルする
-	async cancelChildorder(body) {
-		let method = 'POST';
-		let path = '/v1/me/cancelchildorder';
-		return await this.sendRequest(method, path, body, false);
-	}
+    /* Public Methods*/
+    //資産残高を取得
+    async getBalances() {
+        let method = 'GET';
+        let path = '/v1/me/getbalance';
+        return await this.sendRequest(method, path, null, true);
+    }
 
-	//注文の詳細を取得
-	async getChildorders(id) {
-		let method = 'GET';
-		let path = '/v1/me/getchildorders?product_code=FX_BTC_JPY';
-		if(id) path += `&child_order_acceptance_id=${id}`;
-		return await this.sendRequest(method, path, null, true);
-	}
+    //証拠金状態を取得
+    async getCollateral() {
+        let method = 'GET';
+        let path = '/v1/me/getcollateral';
+        return await this.sendRequest(method, path, null, true);
+    }
 
-	async getBTCBoard() {
-		let method = 'GET';
-		let path = '/v1/board?product_code=BTC_JPY';
-		return await this.sendPublicRequest(method, path, null);
-	}
-	
-	//板情報を取得
-	async getFXBoard() {
-		let method = 'GET';
-		let path = '/v1/board?product_code=FX_BTC_JPY';
-		return await this.sendPublicRequest(method, path, null);
-	}
+    async getHelp() {
 
-	async sendPublicRequest(method, path, body) {
-		let url = URL + path;
-		let res = await fetch(url);
-		return res.json();
-	}
 
-	async sendRequest(method, path, body, isJson) {
-		let ts = Date.now().toString();
-		body = JSON.stringify(body);
-		let uri = URL + path;
-		let text = ts + method + path;
-		if(method == 'POST') text += body;
-		let sign = crypto.createHmac('sha256', API_SECRET).update(text).digest('hex');
-		let options = {
-			url: uri,
-			method: method,
-			headers: {
-				'ACCESS-KEY': API_KEY,
-				'ACCESS-TIMESTAMP': ts,
-				'ACCESS-SIGN': sign,	
-				'Content-Type': 'application/json',
-			}
-		}
-		if(method == 'POST') options.body = body;
-		if(isJson) {
-			let res = await r2(options).json;
-			return res;
-		} else {
-			let res = await r2(options);
-			return res;
-		}
-	}
+    }
+
+    //証拠金変動履歴を取得
+    async getCollateralHistory() {
+        let method = 'GET';
+        let path = '/v1/me/getcollateralhistory';
+        return await this.sendRequest(method, path, null, true);
+    }
+
+    //現在の建玉を取得
+    async getPositions() {
+        let method = 'GET';
+        let path = '/v1/me/getpositions?product_code=FX_BTC_JPY';
+        return await this.sendRequest(method, path, null, true);
+    }
+
+    //注文を出す	
+    async sendChildorder(body) {
+        let method = 'POST';
+        let path = '/v1/me/sendchildorder';
+        return await this.sendRequest(method, path, body, true);
+    }
+
+    //注文をキャンセルする
+    async cancelChildorder(body) {
+        let method = 'POST';
+        let path = '/v1/me/cancelchildorder';
+        return await this.sendRequest(method, path, body, false);
+    }
+
+    //注文の詳細を取得
+    async getChildorders(id) {
+        let method = 'GET';
+        let path = '/v1/me/getchildorders?product_code=FX_BTC_JPY';
+        if (id) path += `&child_order_acceptance_id=${id}`;
+        return await this.sendRequest(method, path, null, true);
+    }
+
+    async getBTCBoard() {
+        let method = 'GET';
+        let path = '/v1/board?product_code=BTC_JPY';
+        return await this.sendPublicRequest(method, path, null);
+    }
+
+    //板情報を取得
+    async getFXBoard() {
+        let method = 'GET';
+        let path = '/v1/board?product_code=FX_BTC_JPY';
+        return await this.sendPublicRequest(method, path, null);
+    }
+
+    async sendPublicRequest(method, path, body) {
+        let url = URL + path;
+        let res = await fetch(url);
+        return res.json();
+    }
+
+    async sendRequest(method, path, body, isJson) {
+        let ts = Date.now().toString();
+        body = JSON.stringify(body);
+        let uri = URL + path;
+        let text = ts + method + path;
+        if (method == 'POST') text += body;
+        let sign = crypto.createHmac('sha256', this.API_SECRET).update(text).digest('hex');
+        let options = {
+            url: uri,
+            method: method,
+            headers: {
+                'ACCESS-KEY': this.API_KEY,
+                'ACCESS-TIMESTAMP': ts,
+                'ACCESS-SIGN': sign,
+                'Content-Type': 'application/json',
+            }
+        }
+        if (method == 'POST') options.body = body;
+        if (isJson) {
+            let res = await r2(options).json;
+            return res;
+        } else {
+            let res = await r2(options);
+            return res;
+        }
+    }
 }
 
 /* Utility methods*/
 const displayJson = (json) => {
-	try {
-		console.log(json);
-	} catch(err) {
-		console.log(err);
-	}
+    try {
+        console.log(json);
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 module.exports.BitFlyer = BitFlyer;
--- bot/vixrsi/vixrsi_bot.js
+++ bot/vixrsi/vixrsi_bot.js
@@ -10,11 +10,8 @@ const SECRET = require(workDir + '/secret.json');
 const Util = require(`${workDir}/utilities/util`).Util;
 const BitFlyer = require(workDir + '/api/bitflyer').BitFlyer;
 const util = new Util();
-const bfAPI = new BitFlyer();
+const bfAPI = new BitFlyer(SECRET.API_KEY, SECRET.API_SECRET);
 
-//Config内容の読み取り
-const API_KEY = SECRET.API_KEY;
-const API_SECRET = SECRET.API_SECRET;
 //TODO: configのjsonを整理
 const CANDLE_SIZE = VIXConfig.trader.candleSize;
 const PD = VIXConfig.vixStrategyConf.pd;
@@ -24,41 +21,52 @@ const leverage = VIXConfig.trader.leverage;
 const LOSSCUT_PERCENTAGE = VIXConfig.trader.losscutPercentage;
 const PROFIT_PERCENTAGE = VIXConfig.trader.profitPercentage;
 
-//定数宣言
-//TODO: API操作部を外部に切り出し
-const ORDER_HTTP_METHOD = 'POST';
-const ORDER_ENTRY_POINT = '/v1/me/sendchildorder';
-const waitMilliSecond = 1000 * 60 * VIXConfig.trader.candleSize;
+//定数定義
 const LOGNAME = 'vixrsi';
 
 //リアルタイムAPIの準備/設定
 const FX_TICKER_CHANNEL = "lightning_ticker_FX_BTC_JPY";
 const STREAM_URI = "https://io.lightstream.bitflyer.com";
+const FX_EXECUTIONS_CHANNEL = 'lightning_executions_FX_BTC_JPY';
+const SPOT_EXECUTIONS_CHANNEL = 'lightning_executions_BTC_JPY';
 const socket = io(STREAM_URI, { transports: ["websocket"] });
 socket.on("connect", () => {
-	socket.emit("subscribe", FX_TICKER_CHANNEL);
+    socket.emit("subscribe", FX_TICKER_CHANNEL);
+    socket.emit("subscribe", FX_EXECUTIONS_CHANNEL);
+    socket.emit("subscribe", SPOT_EXECUTIONS_CHANNEL);
 });
 
+//Tickerの受信
 socket.on(FX_TICKER_CHANNEL, message => {
-	if(bestBid == -1 || bestAsk == -1) {
-		console.log(`BestBidSetTo: ${message.best_bid}, BestAskSetTo: ${message.best_ask}`);
-	}
-	bestBid = message.best_bid;
-	bestAsk = message.best_ask;
+    if (bestBid == -1 || bestAsk == -1) {
+        console.log(`BestBidSetTo: ${message.best_bid}, BestAskSetTo: ${message.best_ask}`);
+    }
+    bestBid = message.best_bid;
+    bestAsk = message.best_ask;
+});
+
+socket.on(FX_EXECUTIONS_CHANNEL, message => {
+    fxBTCJPY = message[0].price;
+});
+
+socket.on(SPOT_EXECUTIONS_CHANNEL, message => {
+    spotBTCJPY = message[0].price;
 });
 
 let bestBid = -1;
 let bestAsk = -1;
 let maxPosition = 0;
+let spotBTCJPY = -1;
+let fxBTCJPY = -1;
 
 ///
 /// 証拠金と評価評価損益からロスカットの可否を返す
 ///
-const checkLosscut = async () => {
-	let collateral = await bfAPI.getCollateral();
-	let amount = collateral.collateral;
-	let pnl = collateral.open_position_pnl;
-	return pnl <= -(amount * (LOSSCUT_PERCENTAGE / 100));
+const checkLosscut = async() => {
+    let collateral = await bfAPI.getCollateral();
+    let amount = collateral.collateral;
+    let pnl = collateral.open_position_pnl;
+    return pnl <= -(amount * (LOSSCUT_PERCENTAGE / 100));
 };
 
 ///
@@ -68,17 +76,17 @@ const checkLosscut = async () => {
 /// 一度認識した後にその額を下回れば損失になる前に利確するスタンス
 ///
 const checkSecureProfit = async(detected) => {
-	let collateral = await bfAPI.getCollateral();
-	let amount = collateral.collateral;
-	let pnl = collateral.open_position_pnl;
-	return result = false;
-	let message = '';
-	if(detected) {
-		result = pnl <= amount * (PROFIT_PERCENTAGE / 100);
-	} else {
-		result = pnl >= amount * (PROFIT_PERCENTAGE / 100);
-	}
-	return result;
+    let collateral = await bfAPI.getCollateral();
+    let amount = collateral.collateral;
+    let pnl = collateral.open_position_pnl;
+    return result = false;
+    let message = '';
+    if (detected) {
+        result = pnl <= amount * (PROFIT_PERCENTAGE / 100);
+    } else {
+        result = pnl >= amount * (PROFIT_PERCENTAGE / 100);
+    }
+    return result;
 };
 
 ///
@@ -86,204 +94,209 @@ const checkSecureProfit = async(detected) => {
 /// 切り捨て(証拠金 / (現在価格 * 発注単価(最小0.01) / レバレッジ(15));
 /// 切り捨て(2000 / (600000 * 0.01 / 15)) => 5
 ///
-const getMaxPosition = async () => {
-	let collateralObj = await bfAPI.getCollateral();
-	let collateral = collateralObj.collateral;
-	console.log(`証拠金:${collateral}円`);
-	let price = await bfAPI.getFXBoard();
-	price = price.mid_price;
-	let unitPrice = price * orderSize / leverage;
-	let result = Math.floor(collateral / unitPrice);
-	console.log(`最大建玉数:${result}`);
-	return result;
+const getMaxPosition = async() => {
+    let collateralObj = await bfAPI.getCollateral();
+    let collateral = collateralObj.collateral;
+    console.log(`証拠金:${collateral}円`);
+    let price = await bfAPI.getFXBoard();
+    price = price.mid_price;
+    let unitPrice = price * orderSize / leverage;
+    let result = Math.floor(collateral / unitPrice);
+    console.log(`最大建玉数:${result}`);
+    return result;
 };
 
-const getSFD = async() => {
-	let fxPrice = (await bfAPI.getFXBoard()).mid_price;
-	let btcPrice = (await bfAPI.getBTCBoard()).mid_price;
-	let kairi = fxPrice/btcPrice * 100 - 100;
-	return kairi;
+///
+/// 現在の現物とFXとの価格乖離(%)を取得する
+/// どちらかの最終取引価格が未受信であれば0を返す
+/// 価格乖離 (%) = （Lightning FX 取引価格 ÷ Lightning 現物 （BTC/JPY）最終取引価格 − 1）× 100
+/// 実装は公式サイトの説明通りの計算式とする
+///
+const getEstrangementPercentage = async() => {
+    if (fxBTCJPY == -1 || spotBTCJPY == -1) return 0;
+    let estrangementPercentage = (fxBTCJPY / spotBTCJPY - 1) * 100;
+    return estrangementPercentage;
 }
 
 // 変更点: メインの処理を関数に切り出し
-const vixRSITrade = async () => {
-	//現在のポジション
-	let position = 'CLOSED';
-	//Strategyから渡されるシグナル
-	let signal = 'HOLD';
-	let order = {
-		product_code: 'FX_BTC_JPY',
-		child_order_type: 'MARKET',
-		price: 0,
-		size: orderSize,
-	};
-	//現在のポジション
-	let currentPosition = signal;
-	//現在の建玉合計
-	let numPosition = 0;
-	//動作間隔 == ローソク足間隔
-	let interval = 60;
-	//建玉所持中フラグ
-	let whilePositioning = false;
-	//利確フラグ
-	let secureProfit = false;
-	//利確認識フラグ
-	let secureProfitDetected = false;
-	//ロスカット時のポジション決済フラグ
-	let positionExited = false;
-	//ロスカットフラグ
-	let losscut = false;
-	//ロスカット認識時のポジション
-	let losscutSignal = '';
-	//ロギング用のメッセージ
-	let logMessage = '';
-	//プログラム開始時に最大ポジション数を算出する
-	maxPosition = await getMaxPosition();
-	console.log(`最大建玉:${maxPosition} で開始します`);
-	//一定時間ごとにポジション移行の判断を行う
-	try {
-		let ohlc = {};
-		while(true) {
-			//ロスカット or 利確をチェック
-			if(whilePositioning) {
-				if(!losscut) {
-					losscut = await checkLosscut();
-				}
-				//Losscutと同時成立は多分あり得ない上awaitで時間の無駄になるのでlosscut成立時はチェックしない
-				if(!losscut) {
-					if(!secureProfitDetected) {
-						secureProfitDetected = await checkSecureProfit(secureProfitDetected);
-					} else {
-						secureProfit = await checkSecureProfit(secureProfitDetected);
-					}
-				}
-			}
-			
-			ohlc = await CwUtil.getOhlc(CANDLE_SIZE, PD + LB);
-			signal = Strategy.vixRsiSignal(ohlc, position);
-			position = Strategy.getNextPosition(position, signal);
-			
-			logMessage = `${moment(Date.now()).format('YYYY/MM/DD HH:mm:ss')} - ${signal},${position}`;
-			console.log(logMessage);
-			util.logging(LOGNAME, logMessage);
-		 	
-			if(signal === 'EXIT' || (losscut && !positionExited) || secureProfit) {
-				if((!losscut && !secureProfit) && (currentPosition === 'NONE' || currentPosition === 'HOLD')) continue;
-				
-				if(currentPosition === 'LONG')  {
-					order.side = 'SELL';
-					losscutSignal = 'BUY';
-				} else if(currentPosition === 'SHORT') {
-					order.side = 'BUY';
-					losscutSignal = 'SELL';
-				}
-				order.size = numPosition * orderSize;
-				order.child_order_type = 'MARKET';
-				order.price = 0;
-			
-				let childOrder = await bfAPI.sendChildorder(order);
-				
-				if(childOrder.child_order_acceptance_id) {
-					logMessage += `ポジション:${position}, 取引枚数:${numPosition * order.size}BTC`;
-					positionExited = true;
-					numPosition = 0.0;
-					currentPosition = 'HOLD';
-					whilePositioning = false;
-					secureProfit = false;
-					secureProfitDetected = false;
-					maxPosition = await getMaxPosition();	
-					
-					if(losscut) logMessage = 'ロスカット';
-					else if(secureProfit) logMessage = '利食い';
-					else logMessage = '手仕舞';
+const vixRSITrade = async() => {
+    //現在のポジション
+    let position = 'CLOSED';
+    //Strategyから渡されるシグナル
+    let signal = 'HOLD';
+    let order = {
+        product_code: 'FX_BTC_JPY',
+        child_order_type: 'MARKET',
+        price: 0,
+        size: orderSize,
+    };
+    //現在のポジション
+    let currentPosition = signal;
+    //現在の建玉合計
+    let numPosition = 0;
+    //動作間隔 == ローソク足間隔
+    let interval = 60;
+    //建玉所持中フラグ
+    let whilePositioning = false;
+    //利確フラグ
+    let secureProfit = false;
+    //利確認識フラグ
+    let secureProfitDetected = false;
+    //ロスカット時のポジション決済フラグ
+    let positionExited = false;
+    //ロスカットフラグ
+    let losscut = false;
+    //ロスカット認識時のポジション
+    let losscutSignal = '';
+    //ロギング用のメッセージ
+    let logMessage = '';
+    //プログラム開始時に最大ポジション数を算出する
+    maxPosition = await getMaxPosition();
+    console.log(`最大建玉:${maxPosition} で開始します`);
+    //一定時間ごとにポジション移行の判断を行う
+    try {
+        let ohlc = {};
+        while (true) {
+            //ロスカット or 利確をチェック
+            if (whilePositioning) {
+                if (!losscut) {
+                    losscut = await checkLosscut();
+                }
+                //Losscutと同時成立は多分あり得ない上awaitで時間の無駄になるのでlosscut成立時はチェックしない
+                if (!losscut) {
+                    if (!secureProfitDetected) {
+                        secureProfitDetected = await checkSecureProfit(secureProfitDetected);
+                    } else {
+                        secureProfit = await checkSecureProfit(secureProfitDetected);
+                    }
+                }
+            }
 
-					util.logging(LOGNAME, logMessage);
-					console.log(childOrder);
-				} else {
-					console.log("何らかのエラーにより決済注文が通りませんでした");
-					console.log(childOrder);
-				}
-			} else if (signal === 'BUY' || signal === 'SELL') {
-				if(!(losscut && losscutSignal == signal) || numPosition < maxPosition) {
-					
-					losscut = false;
-					losscutSignal = '';
-					order.side = signal;
-					if(signal === 'BUY' && bestAsk != -1) {
-						order.child_order_type = 'LIMIT';
-						order.price = bestAsk;
-					} else if(signal === 'SELL' && bestBid != -1) {
-						order.child_order_type = 'LIMIT';
-						order.price = bestBid;
-					}
-					order.size = orderSize
-				
-					let sfd = await getSFD();
-					if(signal === 'SELL' || (signal === 'BUY' && sfd < 4.9)) {
-						let tryOrderCount = 0;
-						while(true) {
-							let childOrder = await bfAPI.sendChildorder(order);
-							if(childOrder.child_order_acceptance_id) {
-								tryOrderCount++;
-								
-								let id = childOrder.child_order_acceptance_id;
-								let result = await waitContractOrderForFiveSec(id);
-								if(result) {
-									numPosition++;
-									currentPosition = position;
-									whilePositioning = true;
-									positionExited = false;
-									logMessage = `シグナル:${signal}, ポジション:${position}, 取引枚数:${order.size}BTC, 約定価格:${order.price}`;
-									console.log(logMessage);
-									util.logging(LOGNAME, logMessage);
-									console.log(childOrder);
-									break;
-								}
-								
-								console.log('注文が5秒間約定しなかったためキャンセルします。');
-								let cancelBody = {
-									product_code: 'FX_BTC_JPY',
-									child_order_acceptance_id: id
-								};
-								bfAPI.cancelChildorder(cancelBody);
-								/*
-								let cancel = await bfAPI.cancelChildorder(cancelBody);
-								console.log(cancel);
-								*/
-								if(tryOrderCount >= 5) break;
-							} else {
-								console.log('エラーにより正常に発注できませんでした');
-								console.log(childOrder);
-								break;
-							}
-						}
-					} 
-				}
-			}
-			await sleepSec(interval * CANDLE_SIZE - 1);
-		}
-	} catch(error) {
-		console.log(error);
-	}
+            ohlc = await CwUtil.getOhlc(CANDLE_SIZE, PD + LB);
+            signal = Strategy.vixRsiSignal(ohlc, position);
+            position = Strategy.getNextPosition(position, signal);
+
+            logMessage = `${moment(Date.now()).format('YYYY/MM/DD HH:mm:ss')} - ${signal},${position}`;
+            console.log(logMessage);
+            util.logging(LOGNAME, logMessage);
+
+            if (signal === 'EXIT' || (losscut && !positionExited) || secureProfit) {
+                if ((!losscut && !secureProfit) && (currentPosition === 'NONE' || currentPosition === 'HOLD')) continue;
+
+                if (currentPosition === 'LONG') {
+                    order.side = 'SELL';
+                    losscutSignal = 'BUY';
+                } else if (currentPosition === 'SHORT') {
+                    order.side = 'BUY';
+                    losscutSignal = 'SELL';
+                }
+                order.size = numPosition * orderSize;
+                order.child_order_type = 'MARKET';
+                order.price = 0;
+
+                let childOrder = await bfAPI.sendChildorder(order);
+
+                if (childOrder.child_order_acceptance_id) {
+                    logMessage += `ポジション:${position}, 取引枚数:${numPosition * order.size}BTC`;
+                    positionExited = true;
+                    numPosition = 0.0;
+                    currentPosition = 'HOLD';
+                    whilePositioning = false;
+                    secureProfit = false;
+                    secureProfitDetected = false;
+                    maxPosition = await getMaxPosition();
+
+                    if (losscut) logMessage = 'ロスカット';
+                    else if (secureProfit) logMessage = '利食い';
+                    else logMessage = '手仕舞';
+
+                    util.logging(LOGNAME, logMessage);
+                    console.log(childOrder);
+                } else {
+                    console.log("何らかのエラーにより決済注文が通りませんでした");
+                    console.log(childOrder);
+                }
+            } else if (signal === 'BUY' || signal === 'SELL') {
+                if (!(losscut && losscutSignal == signal) || numPosition < maxPosition) {
+
+                    losscut = false;
+                    losscutSignal = '';
+                    order.side = signal;
+                    if (signal === 'BUY' && bestAsk != -1) {
+                        order.child_order_type = 'LIMIT';
+                        order.price = bestAsk;
+                    } else if (signal === 'SELL' && bestBid != -1) {
+                        order.child_order_type = 'LIMIT';
+                        order.price = bestBid;
+                    }
+                    order.size = orderSize
+
+                    let sfd = getEstrangementPercentage();
+                    if (signal === 'SELL' || (signal === 'BUY' && sfd < 4.9)) {
+                        let tryOrderCount = 0;
+                        while (true) {
+                            let childOrder = await bfAPI.sendChildorder(order);
+                            if (childOrder.child_order_acceptance_id) {
+                                tryOrderCount++;
+
+                                let id = childOrder.child_order_acceptance_id;
+                                let result = await waitContractOrderForFiveSec(id);
+                                if (result) {
+                                    numPosition++;
+                                    currentPosition = position;
+                                    whilePositioning = true;
+                                    positionExited = false;
+                                    logMessage = `シグナル:${signal}, ポジション:${position}, 取引枚数:${order.size}BTC, 約定価格:${order.price}`;
+                                    console.log(logMessage);
+                                    util.logging(LOGNAME, logMessage);
+                                    console.log(childOrder);
+                                    break;
+                                }
+
+                                console.log('注文が5秒間約定しなかったためキャンセルします。');
+                                let cancelBody = {
+                                    product_code: 'FX_BTC_JPY',
+                                    child_order_acceptance_id: id
+                                };
+                                bfAPI.cancelChildorder(cancelBody);
+                                /*
+                                let cancel = await bfAPI.cancelChildorder(cancelBody);
+                                console.log(cancel);
+                                */
+                                if (tryOrderCount >= 5) break;
+                            } else {
+                                console.log('エラーにより正常に発注できませんでした');
+                                console.log(childOrder);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            await sleepSec(interval * CANDLE_SIZE - 1);
+        }
+    } catch (error) {
+        console.log(error);
+    }
 };
 
-const waitContractOrderForFiveSec = async (id) => {
-	let orders = null;
-	for(let count = 0; count < 5; count++) {
-		orders = await bfAPI.getChildorders(id);
-		if(orders.length && orders[0].child_order_state === 'COMPLETED') return true;
-		await sleepSec(1);
-	}
+const waitContractOrderForFiveSec = async(id) => {
+    let orders = null;
+    for (let count = 0; count < 5; count++) {
+        orders = await bfAPI.getChildorders(id);
+        if (orders.length && orders[0].child_order_state === 'COMPLETED') return true;
+        await sleepSec(1);
+    }
 }
 
-const sleepSec = async (seconds) => {
-	const interval = 1000 * seconds;
-	return new Promise(resolve => setTimeout(resolve, interval));
+const sleepSec = async(seconds) => {
+    const interval = 1000 * seconds;
+    return new Promise(resolve => setTimeout(resolve, interval));
 };
 
 const vixRSIBot = () => {
-	console.log('[稼働開始]');
-	vixRSITrade();
+    console.log('[稼働開始]');
+    vixRSITrade();
 };
 
 vixRSIBot();
