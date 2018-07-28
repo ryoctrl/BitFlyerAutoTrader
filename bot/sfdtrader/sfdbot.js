64cd7606e53e9439425a72a0ade43b1f300b10d9
ryo <ryoctrl@gmail.com>
2018-07-28 10:01:32 +0900

    SFDbotを一新

--- bot/sfdtrader/sfdbot.js
+++ bot/sfdtrader/sfdbot.js
@@ -1,171 +1,112 @@
 const io = require('socket.io-client');
 
 const workDir = process.cwd();
-const config = require(__dirname + '/config.json');
 const BitFlyer = require(`${workDir}/api/bitflyer`).BitFlyer;
 const SECRET = require(workDir + '/secret.json');
 const Util = require('../../utilities/util').Util;
 
-const btcTickerChannel = 'lightning_ticker_BTC_JPY';
-const fxTickerChannel = 'lightning_ticker_FX_BTC_JPY';
-const fxBoardChannel = 'lightning_board_FX_BTC_JPY';
-const fxExecutionsChannel = 'lightning_executions_FX_BTC_JPY';
 const btcExecutionsChannel = 'lightning_executions_BTC_JPY';
 const streamURI = 'https://io.lightstream.bitflyer.com';
 const api = new BitFlyer(SECRET.API_KEY, SECRET.API_SECRET);
 const util = new Util();
-const orderSize = config.amount;
 
 //リアルタイムAPIの準備/設定
 const socket = io(streamURI, { transports: ["websocket"] });
 
 socket.on("connect", () => {
-    socket.emit("subscribe", fxTickerChannel);
     socket.emit("subscribe", btcExecutionsChannel);
 });
 
 
 socket.on(btcExecutionsChannel, message => {
-    btcjpy = message[0].price;
+    sfdPrice = Math.round(message[0].price * 1.05);
 });
 
-socket.on(fxTickerChannel, message => {
-    bestBid = message.best_bid;
-    bestAsk = message.best_ask;
-});
-
-//現物BTC/JPYの最終取引価格
-let btcjpy = 0;
-//現在のbestBid
-let bestBid = 0;
-//現在のbestAsk
-let bestAsk = 0;
-//注文ID
-let executionID = ''
-//現在の建玉合計
-let numPosition = 0;
-//ループ間隔(秒)
-let interval = 0.2;
-//取引処理実行までの閾値
-const countLimit = 10 / interval;
-//取引処理用のカウンタ
-let count = countLimit;
 //注文用obj
 let order = {
     product_code: 'FX_BTC_JPY',
     child_order_type: 'LIMIT',
+    size: 0.01,
 };
 
-const checkingOrder = async(id) => {
-    let count = 0;
-    while (true) {
-        if (count > 5) return false;
-        let orders = await api.getChildorders(id);
-        if (orders.length && orders.length > 0) {
-            if (orders[0].child_order_state === 'COMPLETED') return true;
-        }
-        count++;
-        await util.sleepSec(0.8);
-        continue;
-    }
+let positioning = false;
+let sfdPrice = 0;
+let positioningPrice = 0;
+let positionGetted = false;
+const displaySFDPrice = async() => {
+	while(true) {
+		if(sfdPrice == 0) {
+			await util.sleepSec(1);
+			continue;
+		}
+		
+		if(positioning && positionGetted) {
+			order.price = sfdPrice - 1;
+			order.side = 'BUY';
+			positioning = false;
+			await execOrder(false);
+			console.log('complete buy process');
+		} if(!positioning && !positionGetted) {
+			order.price = sfdPrice;
+			order.side = 'SELL';
+			positioning = true;
+			positioningPrice = order.price;
+			await execOrder(true);
+			console.log('complete sell process');
+		}
+		await util.sleepSec(0.01);
+	}	
 };
 
-const sfdTrade = async() => {
-    while (true) {
-        if (numPosition != 0 && getCurrentSFD() > 5.5) {
-            order.side = 'BUY';
-            order.child_order_type = 'MARKET';
-            order.size = orderSize * numPosition;
-            let o = await api.sendChildorder(order);
-            if (api.child_order_acceptance_id) {
-                order.child_order_type = 'LIMIT';
-                numPosition = 0;
-            }
-
-        }
-        if (numPosition == 0 && count >= countLimit) {
-            order.side = 'SELL';
-            order.price = canGetSFDSell();
-            order.size = orderSize;
-            if (test && order.price != -1) {
-                console.log(`売り: ${order.price}`);
-                numPosition++;
-            } else if (order.price != -1) {
-                console.log(`売り: ${order.price}`);
-                let o = await api.sendChildorder(order);
-                console.log(o);
-                if (o.child_order_acceptance_id) {
-                    executionID = o.child_order_acceptance_id;
-                    numPosition++;
-                    console.log(`SELL: ${numPosition}`);
-                    count = 0;
-                    let completed = await checkingOrder(o.child_order_acceptance_id);
-                    console.log(completed);
-                    if (!completed) {
-                        numPosition = 0;
-                        let body = {
-                            product_code: 'FX_BTC_JPY',
-                            child_order_acceptance_id: executionID
-                        };
-                        let cancel = await api.cancelChildorder(body);
-                        console.log(cancel);
-                    }
-                }
-            }
-        } else if (numPosition > 0) {
-            order.side = 'BUY';
-            order.price = canFreeSFDBuy();
-            order.size = orderSize * numPosition;
-            if (test && order.price != -1) {
-                console.log(`買い戻し: ${order.price}`);
-                numPosition = 0;
-            } else if (order.price != -1) {
-                console.log(`買い戻し: ${order.price}`);
-                let o = await api.sendChildorder(order);
-                if (o.child_order_acceptance_id) {
-                    numPosition = 0;
-                    let completed = await checkingOrder(o.child_order_acceptance_id);
-                    if (!completed) {
-                        numPosition = 1;
-                        let body = {
-                            product_code: 'FX_BTC_JPY',
-                            child_order_acceptance_id: executionID
-                        };
-                        let cancel = await api.cancelChildorder(body);
-                        console.log(cancel);
-                    };
-                }
-            }
-        }
-        count++;
-        await util.sleepSec(0.2);
-    }
+let body = {
+	product_code: 'FX_BTC_JPY',
+	child_order_acceptance_id: ''
 };
 
-let count = 0;
-const canGetSFDSell = () => {
-    let percentage = (bestBid / btcjpy - 1) * 100;
-    console.log(`売り待機中... BestBid: ${bestBid}, 乖離率: ${percentage}`);
-    if (isNaN(percentage) || !isFinite(percentage)) {
-        if (count >= 30) console.log('price always Inifinity!');
-        else count++;
-        return -1;
-    }
+let orderIds = [];
 
-    if (percentage > 5.1) return -1;
-    return percentage > 5.025 ? bestBid : -1;
+const cancelAllorders = async () => {
+	if(orderIds.length == 0) return;
+	let copyOfOrderIds = orderIds.slice();
+	orderIds = [];
+	for(let orderId of copyOfOrderIds) {
+		body.child_order_acceptance_id = orderId;
+		api.cancelChildorder(body)
+		await util.sleepSec(0.5);
+	}
+};
+const execOrder = async (selling) => {
+	console.log('execOrder');
+	while(true) {
+		if((positioning && order.price != sfdPrice) || (!positioning && order.price != sfdPrice)) {
+			cancelAllorders();
+			if(positioning) {
+				console.log('celling price');
+				order.price = sfdPrice;
+			} else {
+				console.log('buying price');
+				order.price = sfdPrice - 1;
+			}
+		} else {
+			await util.sleepSec(1);
+			continue;
+		}
+		let o = await api.sendChildorder(order);
+		console.log(`${order.side}, 0.01:${order.price}`);
+		console.log(o);
+		if(o.child_order_acceptance_id) {
+			let id = o.child_order_acceptance_id;
+			orderIds.push(id);
+			let orders = await api.getCompletedChildorders(id);
+			if(orders.length && orders.length != 0) {
+				positionGetted = selling;
+				console.log('complete order');
+				return;
+			}
+			//body.child_order_acceptance_id = id;
+			//api.cancelChildorder(body);
+		}
+		await util.sleepSec(1);
+	}
 }
-
-const getCurrentSFD = () => {
-    return (bestBid / btcjpy - 1) * 100;
-}
-
-const canFreeSFDBuy = () => {
-    let percentage = (bestAsk / btcjpy - 1) * 100;
-    console.log(`買い待機中... BestAsk: ${bestAsk}, 乖離率: ${percentage}`);
-    if (isNaN(percentage) || !isFinite(percentage)) return -1;
-    return (bestAsk / btcjpy - 1) * 100 < 4.975 ? bestAsk : -1;
-}
-
-
-sfdTrade();
+displaySFDPrice();
